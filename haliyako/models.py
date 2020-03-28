0db42d731028300e29bd4f4e0a90539713e462ea
Osborn Ng'imor <Osborn@Osborns-MacBook-Pro.local>
2020-03-28 22:50:22 +0300

    routing - add new routing levels

--- haliyako/models.py
+++ haliyako/models.py
@@ -5,9 +5,11 @@ from haliyako import  db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone_number = db.Column(db.String(12), nullable=False)
+    other = db.Column(db.String(4), nullable=False)
     age = db.Column(db.String(3), nullable=False)
-    gender = db.Column(db.String(1), nullable=False)
+    sex = db.Column(db.String(1), nullable=False)
+    symp = db.Column(db.String(120), nullable=False)
     time_stamp = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User({self.phone_number}, {self.age}, {self.gender})"
--- haliyako/routes.py
+++ haliyako/routes.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 from haliyako import app, db
 from haliyako.models import User
 
+levels_dict = {}
+
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
@@ -13,7 +15,6 @@ def hello_world():
 
 @app.route('/ussd', methods=['POST', 'GET'])
 def ussd():
-    print("I got here")
     phoneNumber = request.form.get('phoneNumber')
     text = request.form.get('text')
     serviceCode = request.form.get('serviceCode')
@@ -21,39 +22,101 @@ def ussd():
     networkCode = request.form.get('networkCode')
     print(text)
     levels = text.split('*')
-    age = -1;
-    gender = -1;
 
-    response = 'CON Welcome to Hali Yako\n' \
-               'Please enter\n' \
-               '1. Male\n' \
-               '2. Female'
+    con = "CON "
+
+    response = 'Welcome to Hali Yako\n' \
+               'What can we do for you\n' \
+               '1. Self Check your symptoms\n' \
+               '2. Log your movements\n' \
+               '3. Report information'
+    if levels[0] != '':
 
     if levels[0] != '':
-        if len(levels) == 1:
-        #     if levels[0] == "1" or levels[0] == "2":
-        #         gender = levels[0];
-        #     else:
-        #
-            response = 'CON Please enter your age'
-        elif len(levels) == 2:
-            response = "CON Reply according to symptom\n" \
-                       "1  . Fever\n" \
-                       "2. Headache\n" \
-                       "3. if both 1 and 2"
-        elif len(levels) == 3:
-            _gender = levels[0]
-            _age = levels[1]
-            user = User(phone_number=phoneNumber, age=_age, gender=_gender)
-            db.session.add(user)
-            db.session.commit()
-            if levels[2] == '1':
-                response = "END Your risk assment is 50%"
-            elif levels[2] == '2':
-                response = "END Your risk assesment is 65%"
-            elif levels[2] == '3':
-                response = "END Your risk assesment is 95%\n" \
-                           "Report to the nearest health centre"
+        # currlevels = []
+        # for l in levels:
+        #     if len(currlevels) == 0:
+        #         if l in ["1", "2", "3"]:
+        #             currlevels.append(l)
+
+        # check if session id is not in levels_dict. If not present, it means we have not gone past level 1
+        """if sessionId not in levels_dict:
+            if levels[-1] not in ["1", "2", "3"]:
+                response = "Invalid choice! Please try again.\n" \
+                           + response
             else:
-                response = "END you entered invalid number"
+                levels_dict[sessionId] = levels[-1]
+
+                if levels[-1] == "1":
+                    response = symptoms(["1"])
+        else:
+            # get current txt
+            valid_text = levels_dict.get(sessionId)
+            valid_levels = valid_text.split("*")
+
+            if valid_levels[-1] == "1":
+                response = symptoms(valid_levels)
+
+    return con + response"""
+    return response
+
+def symptoms(levels):
+    if len(levels) == 1:
+        response = 'Are you answering for yourself or someone else.\n' \
+                   '1. Myself.\n' \
+                   '2. Someone else.\n'
+    elif len(levels) == 2:
+        response = 'CON Where are you located.\n' \
+                   'Enter your county name or county code.\n'
+        if levels[1] == '2':
+            response = 'CON Where are they located.\n' \
+                       'Enter their county name or county code.\n'
+    elif len(levels) == 3:
+        response = 'CON What is your age.'
+        if levels[1] == '2':
+            response = 'CON What is their age.'
+    elif len(levels) == 4:
+        response = 'CON What is your gender.\n'
+        if levels[1] == '2':
+            response = 'CON What is their gender.\n'
+        response = response + '1. Male\n' \
+                              '2. Female\n'
+
+    elif len(levels) == 5:
+        temp = 'you'
+        if levels[1] == '2':
+            temp = 'they'
+        response = 'CON Are ' + temp + ' experiencing any of these symptoms.\n' \
+                                       'Enter all symptoms ' + temp + 'are experiencing.\n' \
+                                                                      '1. Fever\n' \
+                                                                      '2. Dry cough\n' \
+                                                                      '3. Fatigue\n' \
+                                                                      '4. Shortness of breath\n' \
+                                                                      '5. Sore throat\n' \
+                                                                      '6. Headache\n' \
+                                                                      '7. Nausea\n' \
+                                                                      '0. None'
+    elif len(levels) == 6:
+        if levels[1] == '0':
+            response = "END Thank you for taking part in the survey\n" \
+                       "If you experience any of these symptoms, please retake the survey as soon as possible.\n" \
+                       "Good bye, Tujichunge Pamoja"
+        else:
+            response = "CON How long have you been experiencing these symptoms\n" \
+                       "1. Less than 3 days\n" \
+                       "2. Less than 7 days\n" \
+                       "3. More than 7 days\n"
+    elif len(levels) == 7:
+        temp = 'you'
+        if levels[1] == '2':
+            temp = 'they'
+        response = "CON Do " + temp + " have any of the following underlining conditions.\n" \
+                                      "1. Hypertension\n" \
+                                      "2. Diabetes\n" \
+                                      "3. Cardiovascular\n" \
+                                      "4. Chronic respiratory\n" \
+                                      "5. Cancer\n" \
+                                      "0. None"
+    else:
+        response = "End Thank you you for your participation"
     return response
