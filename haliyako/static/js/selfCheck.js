1d6eb06c6fb04b52b3ad907ffb0586238244c9eb
bk376 <bk376@cornell.edu>
2020-05-07 06:22:49 +0300

    adding progressive web app features

--- haliyako/static/js/selfCheck.js
+++ haliyako/static/js/selfCheck.js
@@ -9,6 +9,17 @@ var postTopic = "postTopic";
 var toaloc = "1";
 var show = false;
 var sideBarOpen = false;
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('./service-worker.js')
+    .then(function(registration) {
+        console.log('Service Worker Registered!');
+        return registration;
+    })
+    .catch(function(err) {
+        console.error('Unable to register service worker.', err);
+    });
+}
 jQuery(document).ready(function( $ ) {
     $('#sidebarCollapse').on('click', function () {
         document.getElementById("sidebar").style.width = "250px";
